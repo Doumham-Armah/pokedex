c8daab14dbfb9e16799746fd0c38076d176a714a
mahmoudhamra <mahmoudhamra@gmail.com>
2021-06-17 01:21:35 +0300

    added images for each pokemon

---
+++ src/components/pokecard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const poki_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+class Pokecard extends Component {
+    render() { 
+        let imgSrc = `${poki_API}${this.props.id}.png`
+        return ( 
+            <div className="Pokecard">
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc}/>
+                <div>{this.props.type}</div>
+                <div>{this.props.base_experience}</div>
+            </div>
+         );
+    }
+}
+ 
+export default Pokecard;
