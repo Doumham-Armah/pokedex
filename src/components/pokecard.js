60cc36e0262b9f6fa5ba910563661566c03c3b31
mahmoudhamra <mahmoudhamra@gmail.com>
2021-06-17 01:31:07 +0300

    added some style

--- src/components/pokecard.js
+++ src/components/pokecard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../pokecard.css'
 
 const poki_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 class Pokecard extends Component {
@@ -8,8 +9,8 @@ class Pokecard extends Component {
             <div className="Pokecard">
                 <h1>{this.props.name}</h1>
                 <img src={imgSrc} alt={this.props.name}/>
-                <div>{this.props.type}</div>
-                <div>{this.props.base_experience}</div>
+                <div>Type: {this.props.type}</div>
+                <div>EXP: {this.props.base_experience}</div>
             </div>
          );
     }
